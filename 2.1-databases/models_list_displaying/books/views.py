c22d2e699ce9eea33acc69cc6fa3887ef1735abd
Aleksey <alekseytep92@gmail.com>
2022-01-19 10:07:15 +0300

    Solve models list displaying

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -17,5 +17,23 @@ def books_view(request):
 def books_date_view(request, dt):
     template = 'books/books_list.html'
     books = Book.objects.filter(pub_date=dt)
-    context = {'books': books}
+    dates = sorted(set(Book.objects.all().values_list('pub_date')))
+
+    current_idx_date = dates.index((dt.date(),))
+    next_idx_date = current_idx_date + 1
+    previous_idx_date = current_idx_date - 1
+
+    if next_idx_date < len(dates):
+        next_date = dates[next_idx_date][0].strftime('%Y-%m-%d')
+    else:
+        next_date = None
+    if previous_idx_date >= 0:
+        previous_date = dates[previous_idx_date][0].strftime('%Y-%m-%d')
+    else:
+        previous_date = None
+
+    context = {'date': dt,
+               'books': books,
+               'next_date': next_date,
+               'previous_date': previous_date}
     return render(request, template, context)
