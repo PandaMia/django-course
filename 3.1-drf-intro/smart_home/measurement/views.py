f7a49991a06ff6e60b1f9006d7f8c99970f6cf35
Aleksey <alekseytep92@gmail.com>
2022-02-04 12:12:27 +0300

    Homework solution

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,27 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+
+from measurement.models import Sensor, Measurement
+from measurement.serializers import SensorDetailSerializer, MeasurementSerializer, SensorSerializer
+
+
+class MeasurementView(ListCreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def post(self, request):
+        sensor_id = int(request.data['sensor'])
+        sensor = Sensor.objects.filter(id=sensor_id)[0]
+        temperature = float(request.data['temperature'])
+        Measurement(sensor=sensor, temperature=temperature).save()
+        return Response({'Message': 'Measurement created!'})
+
+
+class SensorView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
